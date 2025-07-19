2c6edee159295d5b9dc4e2ead4b1bde1af41fd6e
Pazzyel <huangjin6380@gmail.com>
2025-07-19 14:59:31 +0800

    开发了生成全局唯一id的工具类

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,37 @@
 package com.hmdp;
 
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    private ExecutorService es = Executors.newFixedThreadPool(16);
 
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(() -> {
+               for (int j = 0; j < 100; j++) {
+                   long id = redisIdWorker.nextId("order");
+                   System.out.println("id:" + id);
+               }
+               latch.countDown();
+            });
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 }
