9d5f4e2fea34e9822321930650dfb8fafb7281ee
Pazzyel <huangjin6380@gmail.com>
2025-07-19 16:04:02 +0800

    乐观锁解决秒杀券下单超卖问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -59,10 +59,10 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             // 库存不足
             return Result.fail("库存不足！");
         }
-        // 扣减库存
+        // 扣减库存,乐观锁解决，在更新库存的时候检查库存是否>0防止超卖
         boolean success = seckillVoucherService.update()
                 .setSql("stock= stock -1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).gt("stock",0).update();
         if (!success) {
             //扣减失败
             return Result.fail("库存不足！");
